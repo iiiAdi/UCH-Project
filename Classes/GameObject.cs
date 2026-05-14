7e661edaade2f451f4a463428e9473dd5c004890
Adi Makmal <adimak112233@gmail.com>
2026-05-14 16:32:08 +0300

    Add project files.

---
+++ Classes/GameObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UCH_Project
+{
+    public abstract class GameObject
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public abstract void Draw(Graphics g);
+    }
+}
